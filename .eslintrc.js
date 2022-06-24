98939794b044a15a296a76ac0ac0c0396d6c4d5b
Jesse Wright <jesse.wright@anu.edu.au>
2022-06-24 18:03:35 +1000

    chore: fix linter errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
   extends: ["@inrupt/eslint-config-lib"],
+  rules: {
+    // Since we are re-exporting fetch, Request, Response, Headers
+    // shadowing crops up a lot in this package
+    "no-shadow": "off",
+    "global-require": "off",
+    "@typescript-eslint/no-var-requires": "off",
+  },
 };
