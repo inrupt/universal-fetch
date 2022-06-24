47f5111e23aafb7baf730415d9642ac0c86986aa
Jesse Wright <jesse.wright@anu.edu.au>
2022-06-24 20:44:10 +1000

    chore: Write CD tests

--- .github/workflows/cd-packaging-tests/bundler-parcel/index.ts
+++ .github/workflows/cd-packaging-tests/bundler-parcel/index.ts
@@ -1,3 +1,13 @@
-import { sampleModuleFn } from "@inrupt/universal-fetch";
+import dFetch, { fetch, Headers, Request, Response } from "@inrupt/universal-fetch";
 
-console.log(sampleModuleFn());
+console.log(new Request("https://vincentt.inrupt.net/profile/card#me"));
+console.log(new Response());
+console.log(new Headers())
+
+fetch("https://vincentt.inrupt.net/profile/card#me").then(data => {
+  console.log(data);
+})
+
+dFetch("https://vincentt.inrupt.net/profile/card#me").then(data => {
+  console.log(data);
+})
