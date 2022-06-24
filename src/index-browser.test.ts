c768e68f96a971d151326b1335c1ce7a5bd27c72
Jesse Wright <jesse.wright@anu.edu.au>
2022-06-24 18:03:35 +1000

    chore: fix linter errors

--- src/index-browser.test.ts
+++ src/index-browser.test.ts
@@ -1,35 +1,36 @@
-/*
- * Copyright 2022 Inrupt Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
- * Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+//
+// Copyright 2022 Inrupt Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+// Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
 
 // In the browser environment we always expect fetch
 // and the related globals to exist.
-// @ts-ignore
-globalThis.fetch = () => {};
-// @ts-ignore
-globalThis.Headers = () => {};
-// @ts-ignore
-globalThis.Request = () => {};
-// @ts-ignore
-globalThis.Response = () => {};
 
+globalThis.fetch = ((() => {}) as unknown) as typeof fetch;
+globalThis.Headers = ((() => {}) as unknown) as typeof Headers;
+globalThis.Request = ((() => {}) as unknown) as typeof Request;
+globalThis.Response = ((() => {}) as unknown) as typeof Response;
+
+// We need to set the globals before importing the library
+// as this changes whether the library will use the global
+// fetch or import it from undici
+// eslint-disable-next-line import/first
 import * as uniFetch from "./index-browser";
 
 describe("exports", () => {
@@ -50,4 +51,4 @@ describe("exports", () => {
   it("Should have default export as fetch", () => {
     expect(uniFetch.default).toEqual(uniFetch.fetch);
   });
-})
+});
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,37 @@
-/*
- * Copyright 2022 Inrupt Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
- * Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-import type * as undici from 'undici';
+//
+// Copyright 2022 Inrupt Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+// Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+import type * as undici from "undici";
 
-const uniFetch = (
-    typeof globalThis.fetch === 'function' &&
-    typeof globalThis.Headers === 'function' &&
-    typeof globalThis.Request === 'function' &&
-    typeof globalThis.Response === 'function'
-  ) ? globalThis : require('undici') as typeof undici;
+const uniFetch =
+  typeof globalThis.fetch === "function" &&
+  typeof globalThis.Headers === "function" &&
+  typeof globalThis.Request === "function" &&
+  typeof globalThis.Response === "function"
+    ? globalThis
+    : (require("undici") as typeof undici);
 
 export default uniFetch.fetch as typeof globalThis.fetch;
-export const fetch = uniFetch.fetch as typeof globalThis.fetch;
-export const Request = uniFetch.Request as typeof globalThis.Request;
-export const Response = uniFetch.Response as typeof globalThis.Response;
-export const Headers = uniFetch.Headers as typeof globalThis.Headers;
+export const {
+  fetch,
+  Request,
+  Response,
+  Headers,
+} = uniFetch as typeof globalThis;
