471da9bfc65f9d091fad9544726c307b0d79eee6
Jesse Wright <jesse.wright@anu.edu.au>
2022-06-24 20:47:36 +1000

    chore: Update typedoc config

--- .github/workflows/cd-packaging-tests/bundler-parcel/index.ts
+++ .github/workflows/cd-packaging-tests/bundler-parcel/index.ts
@@ -1,13 +1,18 @@
-import dFetch, { fetch, Headers, Request, Response } from "@inrupt/universal-fetch";
+import dFetch, {
+  fetch,
+  Headers,
+  Request,
+  Response,
+} from "@inrupt/universal-fetch";
 
 console.log(new Request("https://vincentt.inrupt.net/profile/card#me"));
 console.log(new Response());
-console.log(new Headers())
+console.log(new Headers());
 
-fetch("https://vincentt.inrupt.net/profile/card#me").then(data => {
+fetch("https://vincentt.inrupt.net/profile/card#me").then((data) => {
   console.log(data);
-})
+});
 
-dFetch("https://vincentt.inrupt.net/profile/card#me").then(data => {
+dFetch("https://vincentt.inrupt.net/profile/card#me").then((data) => {
   console.log(data);
-})
+});
