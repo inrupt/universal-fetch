591987e375010e4746e2940ddb3f35988e14464b
Zwifi <nseydoux@inrupt.com>
2023-03-13 11:44:58 +0100

    Fix/esm require (#160)
    * Import modules instead of require: Using `require` for dynamic import only works in a CJS context, and fails in an ESM context. We use rollup to output both an ESM and a CJS builds, which appropriately transforms top-level imports into `require` when transpiling the TS code. However, `require` calls used in TS code aren't transformed in the ESM output, resulting in the ESM build being unusable outside of Node 18 (where no `require` is necessary).
    
    Using a top-level import will result in the dependency modules to be loaded even in contexts where they aren't required. However, doing a dynamic import is only supported for ESM-only builds, which isn't our case. Since the target is here server-side code, the bundle size isn't as critical, and this deosn't affect the separate browser module.
    
    * Add dependencies to rollup externals: This fixes the issues with rollup and node-fetch types. Having dependencies in the bundle externals is the default config we use elsewhere, because we want the to be dependencies and bundled by the consumer's bundler, rather than being part of the bundle we make available ourselves.
    
    ---------
    
    Co-authored-by: Jesse Wright <63333554+jeswr@users.noreply.github.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,13 @@ The following changes have been implemented but not released yet:
 
 ## [Unreleased]
 
+### Bugfixes
+
+- Fixes ESM build on Node 16: an incorrect use of dynamic imports broke the ESM
+  build on Node 16.
+
+## [1.0.0](https://github.com/inrupt/universal-fetch/releases/tag/v1.0.0) - 2023-03-09
+
 ### New features
 
 Introducing our fetch shim, where we use node-fetch as a polyfill on node 14,
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,41 +1,44 @@
-/**
- * Copyright 2020 Inrupt Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
- * Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+//
+// Copyright 2022 Inrupt Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+// Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
 
 // The following is only possible from Node 18 onwards
 // import pkg from "./package.json" assert { type: "json" };
 
 // Until we only support Node 18+, this should be used instead
-// (see https://rollupjs.org/guide/en/#importing-packagejson) 
-import { createRequire } from 'node:module';
-const require = createRequire(import.meta.url);
-const pkg = require('./package.json');
+// (see https://rollupjs.org/guide/en/#importing-packagejson)
+import { createRequire } from "node:module";
 
 import typescript from "rollup-plugin-typescript2";
 import { readFileSync } from "fs";
+
+const require = createRequire(import.meta.url);
+const pkg = require("./package.json");
 
 // Hack to be able to read tsconfig and append an excludes rule:
 // Note: this does only remove single line comments.
 const tsconfig = JSON.parse(
   readFileSync("./tsconfig.json", "utf-8").replace(/^\s*\/\/\s.*$/gm, "")
 );
+
+const external = [...Object.keys(pkg.dependencies || {})];
 
 const plugins = [
   typescript({
@@ -72,8 +75,8 @@ export default [
         exports: "named",
       },
     ],
-    plugins: plugins,
-    external: [],
+    plugins,
+    external,
   },
   {
     input: "./src/index-browser.ts",
@@ -101,7 +104,7 @@ export default [
         exports: "named",
       },
     ],
-    plugins: plugins,
-    external: [],
+    plugins,
+    external,
   },
 ];
